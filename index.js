03a3a268c56edcc8521b14395b5f971449d73ecc
+ <+>
2024-06-10 23:02:07 -0400

    Listeners

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+function addingEventListener() {const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+    function clickAlert() {
+      alert('I was clicked!');
+    }
 }
+addingEventListener();
